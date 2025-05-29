ba24651930ea55f5df3783cc4c20c4c54ba9baff
Stuart-0728 <1078184866@qq.com>
2025-05-29 23:22:08 +0800

    Create registration.py

---
+++ src/models/registration.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from . import db
+
+class Registration(db.Model):
+    __tablename__ = 'registrations'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'), nullable=False)
+    status = db.Column(db.String(20), default='Pending')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Registration {self.user_id} - {self.activity_id}>'
