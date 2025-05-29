243511f00f872ed06d8c6e70be7bf47a8313b07e
Stuart-0728 <1078184866@qq.com>
2025-05-29 22:04:13 +0800

    Create _init_.py

---
+++ src/_init_.py
@@ -0,0 +1,12 @@
+from .routes.admin import admin_bp
+from .routes.auth import auth_bp
+from .routes.main import main_bp
+from .routes.student import student_bp
+from .routes.utils import utils_bp
+
+def register_blueprints(app):
+    app.register_blueprint(admin_bp, url_prefix='/admin')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(main_bp)
+    app.register_blueprint(student_bp, url_prefix='/student')
+    app.register_blueprint(utils_bp, url_prefix='/utils')
